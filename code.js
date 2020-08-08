bba24d6daefa8dbde5bf9052f1540062864ce154
HildeTheOkayish <hildevandenbrink@outlook.com>
2020-08-08 19:41:32 +0200

    result test

--- code.js
+++ code.js
@@ -14,3 +14,28 @@ for (var i = 0; i < c.length ; i++) {
 
 color = "a".repeat(3 - color.length) + color
 body.style.backgroundColor = "#" + color
+let bands = ["madness", "elbow", "mcr", "muse", "eltonjohn", "davidbowie", "rollingstones", "realbigfish", "queen", "prince", "smashmouth", "blur"]
+var bandsSubmit = document.getElementById("submitBands")
+let bandsInput = document.getElementById("bands")
+bandsInput.onchange = () => {
+	bandsInput.style.borderColor = ""
+}
+
+var puzzle2 = document.getElementById("puzzle2")
+
+bandsSubmit.onclick = () => {
+	var foundBands = bandsInput.value.split(/(\s|\n)/)
+	let succes = true;
+	for (i in bands) {
+		if (!~foundBands.indexOf(bands[i])) {
+			succes = false;
+			break;
+		}
+	}
+	console.log(succes)
+	if (succes) {
+		puzzle2.style.display = "flex"
+	} else {
+		bandsInput.style.borderColor = "red"
+	}
+}
